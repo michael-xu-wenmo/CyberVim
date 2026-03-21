6ddd745b266d106484ab7009d2e5c3974dc74afa
michael-xu-wenmo <wenmoxu@gmail.com>
2026-03-21 19:47:19 +0000

    Config Structured. Added Telescope

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- Configs --
+require("config.lazy") -- setup package manager
+require("config.options") -- setup options
+require("config.line_formating")
+require("config.aesthetics") -- colorscheme
+require("config.fuzzy") -- fuzzy finder
+
+-- Remaps --
+require("remaps.fuzzy")
---
+++ lua/config/line_formating.lua
@@ -0,0 +1,7 @@
+-- relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- tab stop is 4
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
